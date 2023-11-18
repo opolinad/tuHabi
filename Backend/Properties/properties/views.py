eeaed5e213e77c9ec204e20829c9c4774357b3ef
Andrés López <andreslop@gmail.com>
2023-11-18 11:13:08 -0500

    feat(Property): add pagination

--- Backend/Properties/properties/views.py
+++ Backend/Properties/properties/views.py
@@ -2,6 +2,11 @@ from .models import Property
 from rest_framework import generics
 from .serializers import PropertySerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
+class PropertyListPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
 
 class PropertyList(generics.ListAPIView):
     status_to_list = [3, 4, 5]
@@ -9,3 +14,4 @@ class PropertyList(generics.ListAPIView):
     serializer_class = PropertySerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['city', 'year', 'actual_status']
+    pagination_class = PropertyListPagination
