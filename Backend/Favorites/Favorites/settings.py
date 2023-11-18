6069a3a5fd5ee0dc382639eb5717119e7db194ba
Andrés López <andreslop@gmail.com>
2023-11-18 13:43:36 -0500

    refactor(Favorite): add allowed host

--- Backend/Favorites/Favorites/settings.py
+++ Backend/Favorites/Favorites/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-06_k_-$9^wva)te^k+ls#n=sob87&s#85)jt$ng2qtbe7cu)%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['favorites']
 
 
 # Application definition
