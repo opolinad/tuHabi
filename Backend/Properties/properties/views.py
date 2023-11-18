912e456f5f3eafa7f6f1462c003aceb4c25d7ed5
Andrés López <andreslop@gmail.com>
2023-11-18 10:14:05 -0500

    feat(Property): add property server

---
+++ Backend/Properties/properties/views.py
@@ -0,0 +1,12 @@
+from .models import Property
+from rest_framework import filters
+from rest_framework import generics
+from .serializers import PropertySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+class PropertyList(generics.ListAPIView):
+    status_to_list = [3, 4, 5]
+    queryset = Property.objects.filter(actual_status__in=status_to_list, price__gt=0)
+    serializer_class = PropertySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['city', 'year', 'actual_status']
