c7ecfc1a10fd3ead01391e6da910f6abf56ac4c0
Andrés López <andreslop@gmail.com>
2023-11-19 19:21:32 -0500

    feat(Client): show message if there ar no properties

--- Frontend/src/components/cardsContainer/cardsContainer.jsx
+++ Frontend/src/components/cardsContainer/cardsContainer.jsx
@@ -4,18 +4,23 @@ import Card from "../card/card";
 import { get } from "@/services/httpService";
 
 const CardsContainer = async () => {
-  const data = await get(`${process.env.REACT_APP_BACKEND_URL}/properties/`);
+  let url = `${process.env.REACT_APP_BACKEND_URL}/properties/`;
+  const data = await get(url);
 
   return (
     <div className="mx-40 p-3" id="cards-container">
-      {data.results.map((card) => (
-        <Card
-          id={card.id}
-          location={card.city}
-          price={card.price}
-          status={card.actual_status}
-        />
-      ))}
+      {data.results.length ? (
+        data.results.map((card) => (
+          <Card
+            id={card.id}
+            location={card.city}
+            price={card.price}
+            status={card.actual_status}
+          />
+        ))
+      ) : (
+        <p id="no-results-to-show">No hay resultados para mostrar</p>
+      )}
     </div>
   );
 };
